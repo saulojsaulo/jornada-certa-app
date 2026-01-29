7fada496019bcf8bccfb75561e5ef7dc29fa8ba3
Saulo Santos <saulosantosj@gmail.com>
2026-01-29 17:41:01 +0000

    File changes

--- src/components/jornada/VehicleGrid.jsx
+++ src/components/jornada/VehicleGrid.jsx
@@ -111,17 +111,16 @@ export default function VehicleGrid({ veiculos, macrosPorVeiculo, macrosOntemPor
       />
 
       {/* Cabeçalho da grade */}
-      <div className="hidden md:grid grid-cols-16 gap-2 px-4 py-2 text-xs font-medium text-slate-500 uppercase tracking-wider">
-        <div className="col-span-1"></div>
+      <div className="hidden md:grid grid-cols-12 gap-3 px-5 py-3 text-xs font-semibold text-slate-600 uppercase tracking-wider bg-slate-50 rounded-lg border border-slate-200 mb-2">
+        <div className="col-span-1">Gestor</div>
         <div className="col-span-1">Frota</div>
         <div className="col-span-2">Motorista</div>
-        <div className="col-span-1">Gestor</div>
         <div className="col-span-2">Status</div>
         <div className="col-span-1 text-center">Jornada</div>
         <div className="col-span-1 text-center">Disponível</div>
         <div className="col-span-1 text-center">H. Extra</div>
         <div className="col-span-1 text-center">Total</div>
-        <div className="col-span-4 text-right">Alertas</div>
+        <div className="col-span-2 text-right">Alertas</div>
       </div>
 
       {/* Lista de veículos */}
